d0690e59232b45dd2262ecc4cf9ded08fcbbe66b
mlineselle <64280661+mlineselle@users.noreply.github.com>
2022-02-22 19:14:26 -0600

    Add files via upload
    Random story generator using arrays

---
+++ RandomStoryGenerator.js
@@ -0,0 +1,6 @@
+const beginning = ["A long time ago,","In a galaxy far far away,","Upon a shining hill,","Inside the guts of a space worm,","A few seconda ago, ","While Jeremy was singing in the shower,", "Looking into the eyes of a butterfly,", "While eating fried rice,"];
+const middleSetting = ["there lived a small man,", "sat a large monkey,", "in a rocket ship of mac and cheese,", "around a massive campfire,", "within the eyes of a singing catepillar,", "on a tall mountain in the clouds,"];
+const character = ["Jeremy", "John", "Zorbog", "Corban", "King Julian", "Jesus Christ", "Joshua Lewis", "Remy", "Mort"];
+const middleAction = ["ate a large cactus flower", "shot several small ants", "mowed the lawn of the president", "pet the most loving dog", "pooped out a gleaming crown jewel", "destroyed an evil alien lifeform"];
+const end = ["then died in a spectacularly brutal way.", "the end.", "suddenly swimming into the sea, never to be seen again.", "then ran into a wormhole and dissapeared.", "then decided to turn into the most evil man ever.", "becoming the most beautiful chicken ever."]
+
